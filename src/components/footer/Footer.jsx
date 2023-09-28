19442340dd2bd05bd8da70874a1a0690a6533431
sujonphero <sujonsheikh.in@gmail.com>
2023-09-28 14:02:06 +0600

    done routing

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <div>
+            <p>©2023 iamsujon. All right reserved.</p>
+        </div>
+
+        <div></div>
+    </div>
+  )
+}
+
+export default Footer
